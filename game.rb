026c92398769a7fbc07587d41926402059c98be7
Brandon Felty <brcfelty@gmail.com>
2022-05-30 15:53:11 -0600

    adds game functionality

--- game.rb
+++ game.rb
@@ -6,5 +6,24 @@ class Game
     turn = 0
   end
 
-  
+  def new_turn
+    puts "------ NEW TURN -----"
+  end
+
+  def answer
+    print '> '
+  end
+
+  def correct 
+    puts 'YES! You are correct!'
+  end
+
+  def wrong
+    puts "Seriously? No!"
+  end
+
+  def game_over
+    puts "---- GAME OVER -----"
+    puts "Good bye!"
+  end
 end
--- main.rb
+++ main.rb
@@ -2,3 +2,58 @@ require './question'
 require './game'
 require './player'
 
+# q1 = Question.new()
+# pp q1
+
+# q2 = Question.new()
+# pp q2
+
+# p1 = Player.new(1)
+# pp p1
+
+# p2 = Player.new(2)
+# pp p2
+
+# puts p1.lose?
+
+# p1.score = 0
+
+# puts p1.lose?
+
+# g1 = Game.new()
+# pp g1
+
+p1 = Player.new(1)
+p2 = Player.new(2)
+g1 = Game.new()
+
+turn = 0
+
+while (!p1.lose? && !p2.lose?) do 
+  player = (turn % 2 == 0) ? p1 : p2
+  g1.new_turn
+  q = Question.new()
+  puts "#{player.name}: #{q.question}"
+  g1.answer
+
+  answer = gets.chomp.to_i
+
+  if answer == q.answer
+    g1.correct
+    puts "#{p1.name}: #{p1.score}/3 vs #{p1.name}: #{p2.score}/3"
+  else 
+    player.score -= 1
+    g1.wrong
+    puts "#{p1.name}: #{p1.score}/3 vs #{p1.name}: #{p2.score}/3"
+  end
+
+  turn += 1
+end
+
+if (p1.score == 0)
+  puts "#{p2.name} wins with a score of #{p2.score}/3"
+  g1.game_over
+else 
+  puts "#{p1.name} wins with a score of #{p1.score}/3"
+  g1.game_over
+end
